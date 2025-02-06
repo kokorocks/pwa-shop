19bf865cdb90a659a2baeea6b3c3dce3bd58fccb
Ethan <175902687+kokorocks@users.noreply.github.com>
2025-02-06 13:19:07 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installed');
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
